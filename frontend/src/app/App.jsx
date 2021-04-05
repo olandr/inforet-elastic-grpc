26aaa7b26ff85d0afc7eb0bc491dc86137878ece
olandr <sonomis1@gmail.com>
2021-04-05 23:01:05 +0200

    Adds client stub

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,11 +1,35 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
+import { stub } from '../data/client.js';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Google has got nothing on us!</h1>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevents page-reload
+    console.log('submitting');
+
+    let call = stub.QueryES({ query: 'batman' });
+    call.on('data', function (res) {
+      console.log('Result', res);
+    });
+    call.on('end', function () {
+      console.log('end');
+    });
+    call.on('error', function (e) {
+      console.log(e);
+    });
+    call.on('status', function (status) {
+      console.log(status);
+    });
+  };
+
+  return (
+    <div>
+      <h1>Google has got nothing on us!</h1>
+      <form onSubmit={handleSubmit}>
+        <input type='text' id='query' name='query' />
+        <input type='submit' value='Submit' />
+      </form>
+    </div>
+  );
+};
+
+export default App;
--- frontend/src/data/client.js
+++ frontend/src/data/client.js
@@ -1,31 +1,28 @@
-const PROTO_PATH = __dirname + '/../../../protos/data.proto';
-const grpc = require('@grpc/grpc-js');
-const protoLoader = require('@grpc/proto-loader');
-let packageDefinition = protoLoader.loadSync(PROTO_PATH, {
-  keepCase: true,
-  longs: String,
-  enums: String,
-  defaults: true,
-  oneofs: true,
-});
+import { grpc } from '@improbable-eng/grpc-web';
 
-let ir_service = grpc.loadPackageDefinition(packageDefinition).ir.search;
+const { IR } = require('./data_pb_service.js');
+const { QueryRequest, ResultEntry } = require('./data_pb.js');
 
-const main = () => {
-  let client = new ir_service.IR('localhost:5678', grpc.credentials.createInsecure());
-  let call = client.QueryES({ query: 'batman' });
-  call.on('data', function (res) {
-    console.log('Result', res);
+// Simple example wrapper that will make a request to the gRPC backend.
+export const search = () => {
+  var stub = grpc.client(IR.QueryES, { host: 'http://localhost:8080' });
+  console.log(stub);
+
+  var request = new QueryRequest();
+  request.setQuery('Batman');
+
+  stub.start(new grpc.Metadata({ TestKey: 'cv1' }));
+  stub.send(request);
+  stub.onHeaders((headers) => {
+    console.log('onHeaders', headers);
   });
-  call.on('end', function () {
-    console.log('end');
+  stub.onMessage((message) => {
+    console.log('onMessage', message);
   });
-  call.on('error', function (e) {
-    console.log(e);
+  stub.onEnd((status, statusMessage, trailers) => {
+    console.log('onEnd', status, statusMessage, trailers);
   });
-  call.on('status', function (status) {
-    console.log(status);
-  });
+  stub.finishSend();
 };
 
-main();
+search();
