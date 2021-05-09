bdef0b04a8e0e39f029861d86d497344f1be2015
olandr <sonomis1@gmail.com>
2021-05-09 20:36:12 +0200

    Fixes weigh bug

--- backend/src/storage.py
+++ backend/src/storage.py
@@ -7,28 +7,6 @@ class Storage():
 
   DEFAULT_USERS = [
     {
-      'name': 'read wwii',
-      'read_books': ['0316926280', '0738529737', '080931973X', '068480137X', '3822837121', '0691095434', '1594512981'],
-      'rated_books': [],
-    },
-    {
-      'name': 'read+rated wwii',
-      'read_books': ['0316926280', '0738529737', '080931973X', '068480137X', '3822837121', '0691095434', '1594512981'],
-      'rated_books': ['0316926280', '0738529737', '080931973X', '068480137X', '3822837121', '0691095434', '1594512981'],
-    },
-    
-    {
-      'name': 'read JK rowling',
-      'read_books': ['0747581533', '0747573611', '0195798589', '074757166X', '2070612384', '043955490X', '1855496879'],
-      'rated_books': [],
-    },
-    {
-      'name': 'read+rated JK rowling',
-      'read_books': ['0747581533', '0747573611', '0195798589', '074757166X', '2070612384', '043955490X', '1855496879'],
-      'rated_books': [] ,
-    },
-    
-    {
       'name': 'read urdu books',
       'read_books': ['6736366', '184059067X', '1840590742', '1840590815', '077352763X'],
       'rated_books': [],
@@ -57,7 +35,7 @@ class Storage():
     nextID = len(self.users) +1
     userData = {
       'name': username,
-      'data': User(name=username, language_sensibility=10, interest_sensibility=1),
+      'data': User(name=username),
       'read_books': [],
       'rated_books': [],
     }
